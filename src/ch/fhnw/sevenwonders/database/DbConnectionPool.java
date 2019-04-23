195ae0cb842258527e0424b78bd3e16075993633
Lucas Rüesch <lucas.rueesch@gmail.com>
2019-04-23 16:49:16 +0200

    Server timezone anpassung

--- src/ch/fhnw/sevenwonders/database/DbConnectionPool.java
+++ src/ch/fhnw/sevenwonders/database/DbConnectionPool.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 
 // Source: https://stackoverflow.com/questions/39067373/jdbc-and-multithreading
 public class DbConnectionPool {
-	 private String connString = "jdbc:mysql://127.0.0.1:3306/five4sevenwonders?connectTimeout=5000";
+	 private String connString = "jdbc:mysql://127.0.0.1:3306/five4sevenwonders?connectTimeout=5000&serverTimezone=UTC";
 	 private static DbConnectionPool _instance;
 	 
 	 private static final Logger logger = Logger.getLogger("");
