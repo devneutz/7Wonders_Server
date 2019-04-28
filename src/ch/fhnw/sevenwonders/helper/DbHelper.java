f3025086a156a10882e7b05b5bb979ae34016c0e
gabrieldecastilho <44000769+gabrieldecastilho@users.noreply.github.com>
2019-04-28 18:07:00 +0200

    DBHelper und Clientthread ergänzt
    - Ergänzung DBHelper -> addPlayer, doesPlayerexist
    
    - Clientthread -> Message von Client gemäss StatusCode verarbeiten

--- src/ch/fhnw/sevenwonders/helper/DbHelper.java
+++ src/ch/fhnw/sevenwonders/helper/DbHelper.java
@@ -7,54 +7,126 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import ch.fhnw.sevenwonders.database.DbConnectionPool;
 import ch.fhnw.sevenwonders.interfaces.IPlayer;
 
+/**
+ * 
+ * @author Gabriel de Castilho, Joel Neutzner
+ * 
+ *         Diese Klasse erstellt die Tabelle Player in der mysql DB. In dieser
+ *         Klasse werden zudem alle Überprüfungen welche gegen die
+ *         Datenbankinhalte durchgeführt werden müssen abgebildet.
+ *
+ */
 public class DbHelper {
-	
-	private  static final Logger logger = Logger.getLogger("");
-	 
+
+	private static final Logger logger = Logger.getLogger("");
+
+	/*
+	 * In dieser Methode wird die Datenbank initialisiert. Hierbei wird die Tabelle
+	 * "player" erstellt.
+	 */
 	public static void InitDatabase() {
 		try {
 			Connection tmpConnection = DbConnectionPool.getInstance().getConnection();
-			
+
 			Statement tmpCreateTableStm = tmpConnection.createStatement();
 			tmpCreateTableStm.execute("CREATE TABLE IF NOT EXISTS player"
 					+ "					 ( nickname varchar(255) not null, "
-					+ "						password varchar(255) not null, "
+					+ "						password varchar(255) not null, " 
 					+ "						NoOfGames int, "
-					+ "						NoOfVictories int, "
+					+ "						NoOfVictories int, " 
 					+ "						NoOfThirdPlace int,"
-					+ "						NoOfSecondPlace int, "
+					+ "						NoOfSecondPlace int, " 
 					+ "						PRIMARY KEY(nickname) )");
-		}catch(SQLException inEx) {
+		} catch (SQLException inEx) {
 			logger.log(Level.SEVERE, "Error creating table [player]", inEx);
-		}finally {
+		} finally {
 		}
 	}
-	
+
+	/*
+	 * In dieser Methode wird geprüft ob das Passwort des übergebenen Spielers auch
+	 * mit dem Spielerpasswort welches in der DB abgelegt ist übereinstimmt.
+	 */
 	public static boolean isPasswordValid(IPlayer inPlayer) {
 		try {
 			Connection tmpConnection = DbConnectionPool.getInstance().getConnection();
 			String tmpCheckQuery = "SELECT password FROM player WHERE nickname = ?";
-			PreparedStatement tmpCheckStatement = tmpConnection
-											.prepareStatement(tmpCheckQuery, 
-																ResultSet.TYPE_FORWARD_ONLY, 
-																		ResultSet.CONCUR_READ_ONLY);;
+			PreparedStatement tmpCheckStatement = tmpConnection.prepareStatement(tmpCheckQuery,
+					ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			;
 			tmpCheckStatement.setString(1, inPlayer.getName());
 			ResultSet tmpResult = tmpCheckStatement.executeQuery();
-			if(tmpResult.next()) {
+			if (tmpResult.next()) {
 				String tmpPassword = tmpResult.getString("password");
-				return inPlayer.getPassword() == tmpPassword;
-			}else {
+				return inPlayer.getPassword().equalsIgnoreCase(tmpPassword);
+			} else {
 				return false;
 			}
-		}catch(SQLException inEx) {
+		} catch (SQLException inEx) {
 			logger.log(Level.WARNING, "Error trying to validate password!", inEx);
-		}finally {
+		} finally {
 			// TODO Cleanup?
 		}
 		return false;
+	}
+
+	/*
+	 * In dieser Methode wird überprüft ob der übergebene Spieler bereits in der DB
+	 * vorhanden ist.
+	 */
+	public static boolean doesPlayerExist(IPlayer inPlayer) {
+		try {
+
+			Connection tmpConnection = DbConnectionPool.getInstance().getConnection();
+			String tmpCheckQuery = "SELECT nickname FROM player WHERE nickname = ?";
+			PreparedStatement tmpCheckStatement = tmpConnection.prepareStatement(tmpCheckQuery,
+					ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			;
+			tmpCheckStatement.setString(1, inPlayer.getName());
+			ResultSet tmpResult = tmpCheckStatement.executeQuery();
+			if (tmpResult.next()) {
+				String tmpNickname = tmpResult.getString("nickname");
+				return inPlayer.getName().equalsIgnoreCase(tmpNickname);
+			} else {
+				return false;
+			}
+		} catch (SQLException inEx) {
+			logger.log(Level.WARNING, "Error trying to validate existence of player!", inEx);
+		} finally {
+			// TODO Cleanup?
+		}
+		return false;
+
+	}
+
+	/*
+	 * In dieser Methode wird der übergebene Spieler in die Datenbank eingefügt.
+	 */
+	public static void addPlayer(IPlayer inPlayer) {
+		try {
+			Connection tmpConnection = DbConnectionPool.getInstance().getConnection();
+			String tmpCheckQuery = "INSERT INTO player (nickname, password, NoOfGames, NoOfVictories, NoOfThirdPlace, NoOfSecondPlace)"
+					+ " values (?, ?, ?, ?, ?, ?)";
+			PreparedStatement tmpCheckStatement = tmpConnection.prepareStatement(tmpCheckQuery,
+					ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+			;
+			tmpCheckStatement.setString(1, inPlayer.getName());
+			tmpCheckStatement.setString(2, inPlayer.getPassword());
+			tmpCheckStatement.setInt(3, 0);
+			tmpCheckStatement.setInt(4, 0);
+			tmpCheckStatement.setInt(5, 0);
+			tmpCheckStatement.setInt(6, 0);
+
+			tmpCheckStatement.execute();
+
+		} catch (SQLException inEx) {
+			logger.log(Level.WARNING, "Error trying to add player to db!", inEx);
+		} finally {
+			// TODO Cleanup?
+		}
+
 	}
 }
--- src/ch/fhnw/sevenwonders/model/ClientThread.java
+++ src/ch/fhnw/sevenwonders/model/ClientThread.java
@@ -8,8 +8,9 @@ import java.net.Socket;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+import ch.fhnw.sevenwonders.enums.StartupAction;
 import ch.fhnw.sevenwonders.enums.StatusCode;
+import ch.fhnw.sevenwonders.helper.DbHelper;
 import ch.fhnw.sevenwonders.interfaces.ILobby;
 import ch.fhnw.sevenwonders.interfaces.IPlayer;
 import ch.fhnw.sevenwonders.messages.ClientStartupMessage;
@@ -17,50 +18,138 @@ import ch.fhnw.sevenwonders.messages.Message;
 import ch.fhnw.sevenwonders.messages.ServerStartupMessage;
 import ch.fhnw.sevenwonders.models.Player;
 
+/**
+ * 
+ * @author Gabriel de Castilho, Joel Neutzner
+ * 
+ *         Diese Klasse stellt den ClientThread für den Server dar. Hier wird
+ *         jede Client Anfrage separat in einem eigenen Thread verarbeitet.
+ *
+ */
 public class ClientThread extends Thread {
 	private IPlayer player;
 	private ArrayList<IPlayer> opponents;
 	private Socket socket;
 	private final Logger logger = Logger.getLogger("");
-	
+
 	private final int clientId;
-	
+
+	/*
+	 * Konstruktur für den Clientthread
+	 */
 	public ClientThread(Socket inSocket, int inClientId) {
 		this.socket = inSocket;
 		this.clientId = inClientId;
 		this.opponents = new ArrayList<IPlayer>();
 		logger.log(Level.INFO, "New client joined server [Client " + clientId + "]");
 	}
-	
+
 	@Override
 	public void run() {
 		try (ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
 				ObjectInputStream in = new ObjectInputStream(socket.getInputStream());) {
 			while (true) {
-				Object inObject = in.readObject(); 
-				if(inObject != null) {
-					handlingIncomingMessage((Message)inObject, out);
+				Object inObject = in.readObject();
+				if (inObject != null) {
+					handlingIncomingMessage((Message) inObject, out);
 				}
 			}
-		}
-		catch(EOFException inEOFEx) {
-					
-		}catch(Exception inEx) {
+		} catch (EOFException inEOFEx) {
+
+		} catch (Exception inEx) {
 			logger.log(Level.SEVERE, "Error handling Message  [Client " + clientId + "]", inEx);
 		}
 	}
-	
-	private void handlingIncomingMessage(Message inMessage, ObjectOutputStream outputStream) throws IOException, InterruptedException{
+
+	/*
+	 * In dieser Methode werden die eingehenden Client Messages verarbeitet. Anhand
+	 * der StartupAction wird entschieden wie mit der Message umgegangen wird.
+	 */
+	private void handlingIncomingMessage(Message inMessage, ObjectOutputStream outputStream)
+			throws IOException, InterruptedException {
 		if (inMessage instanceof ClientStartupMessage) {
 			logger.log(Level.INFO, "Message received [Client " + clientId + "] - ClientStartupMessage");
-			// TODO Spieler anhand der einkommenden Nachricht authentifizieren etc.
-			ServerStartupMessage tmpMessage = new ServerStartupMessage(((ClientStartupMessage)inMessage).getActionType());
-			tmpMessage.setPlayer(new Player());
-			tmpMessage.setLobbies(new ArrayList<ILobby>());
-			tmpMessage.setStatusCode(StatusCode.Success);
-			outputStream.writeObject(tmpMessage);
-			outputStream.flush();
-			return;
+			ServerStartupMessage tmpMessage = new ServerStartupMessage(
+					((ClientStartupMessage) inMessage).getActionType());
+			IPlayer tmpPlayer = ((ClientStartupMessage) inMessage).getPlayer();
+
+			if (tmpMessage.getActionType() == StartupAction.Login) {
+
+				if (DbHelper.doesPlayerExist(tmpPlayer)) {
+
+					if (DbHelper.isPasswordValid(tmpPlayer)) {
+						tmpMessage.setPlayer(this.player);
+						tmpMessage.setLobbies(new ArrayList<ILobby>());
+						tmpMessage.setStatusCode(StatusCode.Success);
+						logger.log(Level.INFO,
+								"Player logged in successfully [Client " + clientId + "] - ClientStartupMessage");
+						outputStream.writeObject(tmpMessage);
+						outputStream.flush();
+						return;
+					} else {
+						tmpMessage.setStatusCode(StatusCode.LoginFailed);
+						logger.log(Level.WARNING,
+								"Player typed wrong password [Client " + clientId + "] - ClientStartupMessage");
+						outputStream.writeObject(tmpMessage);
+						outputStream.flush();
+						return;
+					}
+
+				} else {
+					tmpMessage.setStatusCode(StatusCode.LoginFailed);
+					logger.log(Level.WARNING,
+							"Player doesn't exists in db [Client " + clientId + "] - ClientStartupMessage");
+					outputStream.writeObject(tmpMessage);
+					outputStream.flush();
+					return;
+
+				}
+
+			}
+
+			if (tmpMessage.getActionType() == StartupAction.Register) {
+
+				if (DbHelper.doesPlayerExist(tmpPlayer)) {
+					tmpMessage.setStatusCode(StatusCode.RegistrationFailed);
+					logger.log(Level.WARNING,
+							"Player already exists in db [Client " + clientId + "] - ClientStartupMessage");
+					outputStream.writeObject(tmpMessage);
+					outputStream.flush();
+					return;
+
+				} else {
+					DbHelper.addPlayer(tmpPlayer);
+					tmpMessage.setPlayer(this.player);
+					tmpMessage.setLobbies(new ArrayList<ILobby>());
+					tmpMessage.setStatusCode(StatusCode.Success);
+					logger.log(Level.INFO,
+							"Player logged in successfully [Client " + clientId + "] - ClientStartupMessage");
+					outputStream.writeObject(tmpMessage);
+					outputStream.flush();
+					return;
+
+				}
+
+			}
+
+			if (tmpMessage.getActionType() == StartupAction.LoginAsGuest) {
+				Player guestPlayer = new Player();
+				guestPlayer.setName("RANDOMNAME");
+
+				tmpMessage.setPlayer(guestPlayer);
+
+				this.player = guestPlayer;
+
+				tmpMessage.setLobbies(new ArrayList<ILobby>());
+				tmpMessage.setStatusCode(StatusCode.Success);
+				logger.log(Level.INFO, "Guest logged in successfully [Client " + clientId + "] - ClientStartupMessage");
+				outputStream.writeObject(tmpMessage);
+				outputStream.flush();
+				return;
+
+			}
+
 		}
+
 	}
 }
