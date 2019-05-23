02f056ec86c55b2d32250882945afd5cae5f1217
Joel N <joel.neutzner@hotmail.com>
2019-05-23 13:54:36 +0200

    Kommentare hinzugefügt - ServerGame

--- src/ch/fhnw/sevenwonders/model/Game.java
+++ src/ch/fhnw/sevenwonders/model/Game.java
@@ -7,15 +7,12 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
-import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import ch.fhnw.sevenwonders.enums.Age;
 import ch.fhnw.sevenwonders.enums.LobbyAction;
-import ch.fhnw.sevenwonders.enums.StatusCode;
 import ch.fhnw.sevenwonders.helper.DbHelper;
 import ch.fhnw.sevenwonders.helper.InitHelper;
 import ch.fhnw.sevenwonders.interfaces.IBoard;
@@ -25,27 +22,48 @@ import ch.fhnw.sevenwonders.interfaces.IPlayer;
 import ch.fhnw.sevenwonders.messages.Message;
 import ch.fhnw.sevenwonders.messages.ServerLobbyMessage;
 
+/**
+ * Thread für die Verwaltung von Verbindungen - Server-Funktionalität
+ * @author joeln
+ *
+ */
 public class Game extends Thread{
 	private final Logger logger = Logger.getLogger("");
 	private ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
 	private List<ILobby> lobbies;
+	
+	// Statischer Gäste-Counter
 	private static int clientId = 1;
 	
 	private ArrayList<ICard> listOfAllCards = new ArrayList<ICard>();
 	private ArrayList<IBoard> listOfAllBoards = new ArrayList<IBoard>();
 	
+	/**
+	 * Konstruktor
+	 */
 	public Game() {
 		super("ServerSocket");
+		// Initialisieren des Kartenstapels
 		listOfAllCards = InitHelper.initAllCards();
+		
+		// Initialisieren der Boards
 		listOfAllBoards = InitHelper.initAllBoards();
+		
+		// Neue Thread-Safe list
 		this.lobbies = Collections.synchronizedList(new ArrayList<ILobby>());
+		
+		// Anlegen der Datenbanktabellen etc.
 		DbHelper.InitDatabase();
 	}
 
+	/**
+	 * Verwaltet Verbindungen von Clients
+	 */
 	@Override
 	public void run() {
 		try (ServerSocket listener = new ServerSocket(50000, 10, null)) {
 			logger.log(Level.INFO, "Server gestartet [Port 50000] - Warte auf eingehende Verbindungen");
+			// Maximale Anzahl der Clients begrenzen um zu hohe Last zu vermeiden
 			ExecutorService threadPool = Executors.newFixedThreadPool(20);
 			while (true) {
 				Socket socket = listener.accept();
@@ -60,14 +78,34 @@ public class Game extends Thread{
 		}
 	};
 
+	/**
+	 * Getter für Kartenstapel
+	 * @return
+	 */
 	public ArrayList<ICard> getListOfCards(){
 		return this.listOfAllCards;
 	}
 	
+	/**
+	 * Getter für Boards
+	 * @return
+	 */
 	public ArrayList<IBoard> getListOfBoards(){
 		return this.listOfAllBoards;
 	}
 	
+	/**
+	 * Getter für Lobbies
+	 * @return
+	 */
+	public ArrayList<ILobby> getLobbies(){
+		return new ArrayList<ILobby>(this.lobbies);
+	}
+	
+	/**
+	 * Schickt eine bestimmte Message an alle Spieler - neue Lobby erstellt o.ä.
+	 * @param inMessage
+	 */
 	public void broadcastMessage(Message inMessage) {
 		logger.log(Level.INFO, "BROADCASTING MESSAGE");
 		for(ClientThread ct : clients) {
@@ -80,14 +118,24 @@ public class Game extends Thread{
 		}
 	}
 	
+	/**
+	 * Hinzufügen einer Lobby zum Spiel
+	 * @param inLobby
+	 */
 	public void addLobby(ILobby inLobby) {
 		synchronized(this.lobbies){
 			this.lobbies.add(inLobby);
 		}
 	}
 	
+	/**
+	 * Entfernen einer Lobby aus dem Spiel
+	 * @param inLobby
+	 */
 	public void removeLobby(ILobby inLobby) {
+		// Synchronized nötig? Ist eigentlich thread-safe list
 		synchronized(this.lobbies) {
+			// Erstellen eines iterators, damit das entfernen einer Lobby aus der liste funktioniert
 			Iterator<ILobby> iter = this.lobbies.iterator();
 			while(iter.hasNext()) {
 				ILobby L = iter.next();
@@ -99,15 +147,20 @@ public class Game extends Thread{
 		}
 	}
 	
-	public ArrayList<ILobby> getLobbies(){
-		return new ArrayList<ILobby>(this.lobbies);
-	}
-	
-	
+	/**
+	 * Wie viele Spieler befinden sich bereits in der angegebenen Lobby?
+	 * @param lobby
+	 * @return
+	 */
 	public int countLobbyPlayers(ILobby lobby){
 		return getPlayersForLobby(lobby).size();
 	}
 	
+	/**
+	 * Gibt eine Liste von ClientThreads für eine Lobby zurück. Wird benötigt, um ein Spiel zu starten und somit Kartenstapel korrekt weiterzugeben etc.
+	 * @param inLobby
+	 * @return
+	 */
 	public ArrayList<ClientThread> getPlayersForLobby(ILobby inLobby){
 		ArrayList<ClientThread> tmpPlayers = new ArrayList<ClientThread>();
 		synchronized(this.clients) {
@@ -124,12 +177,18 @@ public class Game extends Thread{
 		return tmpPlayers;
 	}
 	
+	/**
+	 * Entfernen eines Clients - beim Schliessen eines Fensters zb.
+	 * @param inClient
+	 */
 	public void removeClient(ClientThread inClient) {
 		synchronized(this.clients) {
 			this.clients.remove(inClient);
 		}
 		
+		// Befindet er sich in einer Lobby, müssen die betroffenen Spieler auch informiert werden.
 		if(inClient.getPlayer().getLobby() != null) {		
+			// Handelt es sich sogar um den Lobby-Master wird die Lobby gelöscht
 			if (inClient.getPlayer().getLobby().getLobbyMaster().getName().equals(inClient.getPlayer().getName())) {
 				this.removeLobby(inClient.getPlayer().getLobby());
 				ServerLobbyMessage tmpBroadcast = new ServerLobbyMessage(LobbyAction.LobbyDeleted);
@@ -158,6 +217,12 @@ public class Game extends Thread{
 		}
 	}
 	
+	/**
+	 * Senden einer Message an einen bestimmten Spieler
+	 * @param inPlayer
+	 * @param inMessage
+	 * @throws IOException
+	 */
 	public void sendMessageToPlayer(IPlayer inPlayer, Message inMessage) throws IOException {
 		synchronized(this.clients) {
 			for(int x = 0; x < this.clients.size(); x++) {
